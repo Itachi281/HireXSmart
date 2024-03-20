97de5e7f4ca966a46c2732d4e4f3a34aae937008
Itachi281 <rishabh.vashisthaa@gmail.com>
2024-03-20 17:58:11 +0530

    new webpage add

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-function openPopup() {
-    document.getElementById("popupForm").style.display = "block";
-  }
-  
-  function closePopup() {
-    document.getElementById("popupForm").style.display = "none";
-  }
-  
-  // Close the pop-up form if the user clicks outside of it
-  window.onclick = function(event) {
-    var popup = document.getElementById("popupForm");
-    if (event.target == popup) {
-      popup.style.display = "none";
+/**
+ * 
+ * @description all javascript code for the site is contained in this file.
+ */
+
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
   }
+}
+
+const container = document.getElementById('container');
+        const overlaycon = document.getElementById('overlayCon');
+        const overlaybtn = document.getElementById('overlayBtn');
+
+        overlaybtn.addEventListener('click', () => {
+            container.classList.toggle('right-panel-active');
+
+            overlaybtn.classList.remove('btnScaled');
+            window.requestAnimationFrame(() => {
+              overlaybtn.classList.add('btnScaled');
+            })
+        });
